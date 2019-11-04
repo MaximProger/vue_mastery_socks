42b429b7b7529383e7f5caf105e3052303abbc25
Maxim Koshman <maksimkoshman090@gmail.com>
2019-11-04 09:33:55 +0300

    add some thinks

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: "Vue Mastery",
         product: 'Socks',
-        image: './img/vmSocks-green.jpg'
+        selectedVariant: 0,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: './img/vmSocks-green.jpg',
+                variantQuantity: 10,
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: './img/vmSocks-blue.jpg',
+                variantQuantity: 0,
+            }
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
     }
 })
